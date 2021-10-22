08e10e1bdbb494904f9c489efe124eca93276432
surajkumar85 <49224173+surajkumar85@users.noreply.github.com>
2021-10-22 22:04:52 +0530

    Wrong output

--- ads/views.py
+++ ads/views.py
@@ -85,12 +85,7 @@ def stream_file(request, pk):
     return response
 
 
-class CommentCreateView(LoginRequiredMixin, View):
-    def post(self, request, pk) :
-        f = get_object_or_404(Ad, id=pk)
-        comment = Comment(text=request.POST['comment'], owner=request.user, ad=f)
-        comment.save()
-        return redirect(reverse('ads:ad_detail', args=[pk]))
+
 
 class CommentDeleteView(OwnerDeleteView):
     model = Comment
